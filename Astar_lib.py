9a993df746c4b758a4b51fe3f5b8f78f370e3281
S.Yang <847410498@qq.com>
2020-04-02 22:50:30 +0800

    pcell编译器更新，cross布线算法更新

--- Astar_lib.py
+++ Astar_lib.py
@@ -1,4 +1,4 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 # coding: utf-8
 
 # In[1]:
@@ -13,7 +13,7 @@ from Netlist_lib import *
 from Param_lib import *
 
 
-# In[2]:
+# In[1]:
 
 
 from random import randint
@@ -63,7 +63,77 @@ class Map():
                 self.map[y][x]=1
             else:
                 self.map[y][x]=2
-            
+
+    def setBlock_cross(self,block_list):
+        flag=0
+        block_list_len=len(block_list)
+        x_first=block_list[0][0]
+        y_first=block_list[0][1]
+        self.map[y_first][x_first]=1
+        x_last=block_list[block_list_len-1][0]
+        y_last=block_list[block_list_len-1][1]
+        self.map[y_last][x_last]=1
+        for i in range(1,block_list_len-1):
+            x_prev=block_list[i-1][0]
+            y_prev=block_list[i-1][1]
+            x_next=block_list[i+1][0]
+            y_next=block_list[i+1][1]
+            x=block_list[i][0]
+            y=block_list[i][1]
+            if(self.map[y][x]==2):
+                #print("rule 1 {0},{1}".format(x,y))
+                self.map[y][x]=3
+                if(abs(x_prev-x_next)==2):
+                    self.map[y_prev][x_prev]=3
+                    self.map[y_next][x_next]=3
+                elif(abs(y_prev-y_next)==2):
+                    self.map[y_prev][x_prev]=3
+                    self.map[y_next][x_next]=3
+            elif(abs(x_prev-x_next)==1 and abs(y_prev-y_next)==1 and self.map[y_prev][x_prev]==3):
+                self.map[y][x]=4
+            elif(self.map[y_prev][x_prev]==4):
+                self.map[y][x]=3
+                flag=1
+            elif(abs(y_prev-y)==1 and self.map[y_prev][x_prev-1]==1 and self.map[y_prev][x_prev+1]==1 and self.map[y_prev][x_prev]==3):
+                self.map[y][x]=3
+                #print("rule 2 {0},{1}".format(x,y))
+            elif(abs(x_prev-x)==1 and self.map[y_prev-1][x_prev]==1 and self.map[y_prev+1][x_prev]==1 and self.map[y_prev][x_prev]==3):
+                self.map[y][x]=3
+                #print("rule 3 {0},{1}".format(x,y))
+
+            elif(abs(x_prev-x_next)==1 and abs(y_prev-y_next)==1):
+                #print("rule 4 {0},{1}".format(x,y))
+                self.map[y][x]=1
+            elif(self.map[y_prev][x_prev]==2):
+                #print("rule 5 {0},{1}".format(x,y))
+                self.map[y][x]=1
+            elif(flag==1):
+                self.map[y][x]=3
+                flag=0
+            else:
+                #print("rule 7 {0},{1}".format(x,y))
+                self.map[y][x]=2
+        for i in range(1,block_list_len-1):
+            x_prev=block_list[i-1][0]
+            y_prev=block_list[i-1][1]
+            x_next=block_list[i+1][0]
+            y_next=block_list[i+1][1]
+            x=block_list[i][0]
+            y=block_list[i][1]
+            if(abs(x_prev-x_next)==1 and abs(y_prev-y_next)==1 and (self.map[y][x]==3 or self.map[y][x]==4)):
+                self.map[y_prev][x_prev]=3
+    def read_path_type(self,path):
+        len_path=len(path)
+        line_info=[]
+        for i in range(0,len_path):
+            x=path[i][0]
+            y=path[i][1]
+            line_info.append(self.map[y][x])
+            if(self.map[y][x]==3 or self.map[y][x]==4):
+                self.map[y][x]=1
+        return line_info
+    
+    
     def clrBlock(self,block_list): #清除block list
         block_list_len=len(block_list)
         for i in range(0,block_list_len):
@@ -172,7 +242,6 @@ def AStarSearch(map, source, dest):
     #mark the found path at the map
     path_temp=[]
     while location is not None:
-        map.map[location.y][location.x] = 2
         #print(location.x,location.y)
         path_temp.append([location.x,location.y])
         location = location.pre_entry
@@ -180,7 +249,7 @@ def AStarSearch(map, source, dest):
     return path
 
 
-# In[5]:
+# In[3]:
 
 
 def map_info(file_dir): #从routing信息和netlist信息来获取制作map的参数
@@ -212,63 +281,75 @@ def map_info(file_dir): #从routing信息和netlist信息来获取制作map的�
 #map_info(File_dir)
 
 
-# In[7]:
+# In[5]:
 
 
-def mono_map_search(file_dir,lib,cell,display):#单层布线函数
-    info=map_info(file_dir)#通过map_info函数整理所需的信息
+def mono_map_search(file_dir,lib,cell,display):
+    info=map_info(file_dir)
     width=info[0]
     height=info[1]
-    layout_map=Map(width,height)#创立一个单map
+    layout_map=Map(width,height)
     #layout_map.showMap()
     layout_info=info[2]
     len_layout=len(layout_info)
     map_origin=info[3]
-    for i in range(0,len_layout):#开始设置map的block点
-        block_point=origin_to_blockpoint(layout_info[i].area,layout_info[i].xy,layout_info[i].orient)#设置block点的原始参考坐标
-        abs_point=get_abs_block_point(map_origin,block_point)#获取绝对坐标
-        layout_map.setBlock(abs_point)#封锁inst所在的点
+    for i in range(0,len_layout):
+        #print(layout_info[i].area)
+        block_point=origin_to_blockpoint(layout_info[i].area,layout_info[i].xy,layout_info[i].orient)
+        #print(block_point)
+        abs_point=get_abs_block_point(map_origin,block_point)
+        #print(abs_point)
+        layout_map.setBlock(abs_point)
+        #layout_map.showMap()
+    #layout_map.showMap()
     #开始寻路
+    #print(coord_info)#提示一下要连线的路径
     coord_info=info[4]
     connection_info=info[5]
     coord_len=len(coord_info)
-    text=open("./creatinst.il",'w+')#打开一个creatinst.il文件用于skill读取
+    text=open("./createinst.il",'w+')
     cellid='''cellID=dbOpenCellViewByType("{0}" "{1}" "layout" "maskLayout")'''.format(lib,cell)#这里以后改，dbopen函数
     print(cellid,file=text)
-    for i in range(0,coord_len):#????coord怎么和layout def里面的不一样
-        #print(coord_info[i])
+    for i in range(0,coord_len):#封锁端口，先把所有端口都封上，在布每一条线的时候再单独打开（clrBlock函数）
         port_1_temp=port_coord_to_map(coord_info[i][0][0],coord_info[i][0][1])#获取第一个端口坐标 map中的
-        #print(port_1_temp)
         port_1=[port_1_temp[0]-map_origin[0],port_1_temp[1]-map_origin[1]]#获取第一个端口的绝对坐标 map中的
-        #print(port_1)
         port_2_temp=port_coord_to_map(coord_info[i][1][0],coord_info[i][1][1])#同上
-        #print(port_2_temp)
         port_2=[port_2_temp[0]-map_origin[0],port_2_temp[1]-map_origin[1]]#同上上
-        #print(port_2)
+        block_source=[port_2[0],port_2[1]]#获取出发点
+        block_dest=[port_1[0],port_1[1]]#获取终点
+        layout_map.setBlock([block_source,block_dest])
+    #layout_map.showMap()
+    for i in range(0,coord_len):#????coord怎么和layout def里面的不一样
+        port_1_temp=port_coord_to_map(coord_info[i][0][0],coord_info[i][0][1])#获取第一个端口坐标 map中的
+        port_1=[port_1_temp[0]-map_origin[0],port_1_temp[1]-map_origin[1]]#获取第一个端口的绝对坐标 map中的
+        port_2_temp=port_coord_to_map(coord_info[i][1][0],coord_info[i][1][1])#同上
+        port_2=[port_2_temp[0]-map_origin[0],port_2_temp[1]-map_origin[1]]#同上上
+        layout_map.clrBlock([[port_2[0],port_2[1]],[port_1[0],port_1[1]]])#先清除掉要布线的这条路径的端口block，不然就找不到valid path
         source=(port_2[0],port_2[1])#获取出发点
         dest=(port_1[0],port_1[1])#获取终点
         path=AStarSearch(layout_map,source,dest)#A星算path
-        #print(path)
-        #layout_map.showMap()
         layout_map.setBlock(path)#把这条path添加到库中
         temp=[coord_info[i][1],coord_info[i][0]]#只能用temp来重新排一下coord_info了
-        #print(temp)
         new_path=[]
         len_path=len(path)
         for k in range(0,len_path):
             new_path.append([path[k][0]+map_origin[0],path[k][1]+map_origin[1]])#把还原的path坐标写入new path中
-        #print(new_path)
-        script=path_to_inst(new_path,temp,0,connection_info[i][0]+'_'+connection_info[i][2])#输入path 获取dbcreate脚本
+        script=path_to_inst(new_path,temp,0,connection_info[i][0]+'_'+connection_info[i][2],0)#输入path 获取dbcreate脚本
         len_script=len(script)
         for j in range(0,len_script):
             print(script[j],file=text)#将dbcreate脚本字符串写进text中输出
-        if(display==1):
+        if(display==1):#显示函数，如果电路规模过大建议display置零
             print("source:{0} to destination:{1}".format(source,dest))
             print("path:{0}".format(new_path))
             layout_map.showMap()
+        layout_map.setBlock([[port_2[0],port_2[1]],[port_1[0],port_1[1]]])
     text.close()
 #display=1
 #mono_map_search("whatever.v",'ysc_layout2','auto_route',display)
+
+
+# In[1]:
+
 
 def multi_map_search(file_dir,lib,cell,display,layer_num):
     info=map_info(file_dir)
@@ -348,8 +429,30 @@ def multi_map_search(file_dir,lib,cell,display,layer_num):
         for j in range(0,len_script):
             print(script[j],file=text)#将dbcreate脚本字符串写进text中输出
     text.close()    
-# In[3]:
 
+
+# In[6]:
+
+
+#测试模块
+'''map_width=10
+map_height=10
+map2=Map(map_width,map_height)
+map2.setBlock([[1,1],[1,2],[1,3],[2,1],[2,2],[2,3],[3,1],[3,2],[3,3],[2,5],[2,6],[3,5],[3,6],[8,3],[9,3],[9,4],[8,4]])
+source1=(4,2)
+dest1=(7,3)
+source2=(4,5)
+dest2=(7,4)
+path1=AStarSearch(map2,source1,dest1)
+map2.showMap()
+print(path1)
+map2.setBlock(path1)
+path2=AStarSearch(map2,source2,dest2)
+map2.showMap()
+print(path2)'''
+
+
+# In[ ]:
 
 
 
